5f52df4f3551cde7ed3137925d96cb6a4d8febf6
LiangMei-Leon <77693263+LiangMei-Leon@users.noreply.github.com>
2023-01-24 22:37:03 -0500

    Add a square and a line

--- DATT3701_Project/Assets/Scripts/ScriptTest.cs
+++ DATT3701_Project/Assets/Scripts/ScriptTest.cs
@@ -7,7 +7,7 @@ public class ScriptTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Liang Mei added a line.
     }
 
     // Update is called once per frame
