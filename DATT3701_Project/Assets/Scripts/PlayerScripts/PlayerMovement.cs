869e011294ed29d0c41167c108afd42e95091537
LiangMei-Leon <77693263+LiangMei-Leon@users.noreply.github.com>
2023-01-27 16:58:37 -0500

    Basic Movement and test scene
    two scripts added, this is a basic movement control version, I will remove and add some detailed features later

---
+++ DATT3701_Project/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 20f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get keyboard/controller input:left as -1 and right as 1
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
