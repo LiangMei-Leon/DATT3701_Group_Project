8da034f5461ab0dffdc247cbf53768908d973add
LiangMei-Leon <77693263+LiangMei-Leon@users.noreply.github.com>
2023-01-30 12:13:28 -0500

    update playercontroller2D value(jumpforce)

--- DATT3701_Project/Assets/Scripts/PlayerScripts/CharacterController2D.cs
+++ DATT3701_Project/Assets/Scripts/PlayerScripts/CharacterController2D.cs
@@ -20,7 +20,6 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] public float jumpInputBufferTime = 0.2f;
 
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	private bool m_Grounded;            // Whether or not the player is grounded.
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
@@ -67,29 +66,9 @@ public class CharacterController2D : MonoBehaviour
 		}
     }
 
-	private void FixedUpdate()
-	{
-		bool wasGrounded = m_Grounded;
-		m_Grounded = false;
-
-		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-		for (int i = 0; i < colliders.Length; i++)
-		{
-			if (colliders[i].gameObject != gameObject)
-			{
-				m_Grounded = true;
-				if (!wasGrounded)
-					OnLandEvent.Invoke();
-			}
-		}
-	}
-
-
 	public void Move(float move, bool jump)
 	{
-		//only control the player if grounded or airControl is turned on m_Grounded || 
+		//only control the player if grounded or airControl is turned on 
 		if (m_AirControl)
 		{
 			/*
