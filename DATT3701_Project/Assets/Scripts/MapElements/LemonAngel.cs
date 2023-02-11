9102ed0de4563771b7b6e0cfe05d61c0b6d03155
LiangMei-Leon <77693263+LiangMei-Leon@users.noreply.github.com>
2023-02-10 22:14:33 -0500

    added fear system

---
+++ DATT3701_Project/Assets/Scripts/MapElements/LemonAngel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LemonAngel : MonoBehaviour
+{
+    private GameObject playerManager;
+    private PlayerEmotionStatus playerEmotion;
+    private float emotionStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerManager = GameObject.FindWithTag("PlayerManager");
+        playerEmotion= playerManager.GetComponent<PlayerEmotionStatus>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player")){
+            playerEmotion.ReturnNormal();
+            Destroy(gameObject);
+        }
+    }
+}
