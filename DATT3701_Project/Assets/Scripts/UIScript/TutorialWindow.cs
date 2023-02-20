77640aeecac4353dedc00194c5289bef45113ab5
Dongmei6661 <w1261941475@gmail.com>
2023-02-20 18:09:54 -0500

    Basic function of Tutorial panel

--- DATT3701_Project/Assets/Scripts/UIScript/TutorialWindow.cs
+++ DATT3701_Project/Assets/Scripts/UIScript/TutorialWindow.cs
@@ -7,45 +7,7 @@ using System;
 
 public class TutorialWindow : MonoBehaviour
 {
-    [Header("Header")]
-    [SerializeField]
-    private Transform headerArea;
-    [SerializeField]
-    private TextMeshProUGUI titleField;
-    
-    [Header("Content")]
-    [SerializeField]
-    private Transform contentArea;
-    [SerializeField]
-    private Image contentImage;
-
-
-    [Header("NextButton")]
-    [SerializeField]
-    private Image NextImage;
-    
-    [Header("BackButton")]
-    [SerializeField]
-    private Image BackImage;
-
-    [Header("CloseButton")]
-    [SerializeField]
-    private Image CloseImage;
-
-
-    private Action onNext;
-    private Action onBack;
-    private Action onClose;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public Button NextButton;
+   public Button BackButton;
+   public Button CloseButton;
 }
